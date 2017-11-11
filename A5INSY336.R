aaccbcc75bb5234b0358cc30f94a65704e538177
Parth Khanna <parth.khanna@mail.mcgill.ca>
2017-11-11 16:59:45 -0500

    a5final

--- A5INSY336.R
+++ A5INSY336.R
@@ -25,7 +25,7 @@ new_data_frame = dplyr::mutate(new_data_frame, diff_budget = (budget - `mean(bud
 #7
 diff_vote = c(new_data_frame$diff_vote)
 diff_budget = c(new_data_frame$diff_budget)
-cor(diff_vote, diff_budget)
+correlation = cor(diff_vote, diff_budget)
 
 #----------------------------Part II---------------------------------------
 
@@ -37,16 +37,17 @@ data_set_2 = subset(data_set_2, (!is.null(budget)))
 
 #2
 new_data_frame_2 = data_set_2 %>% mutate(rank = dense_rank(budget))
-num_rank = dplyr::count(new_data_frame_2, rank)
-new_data_frame_3 = dplyr::mutate(new_data_frame_2, percentile = (rank/num_rank)*100)
-top_90 = dplyr::filter(new_data_frame_3, new_data_frame_3$percentile >= 90)
+#num_rank = dplyr::count(new_data_frame_2, rank)
+#num_rank is a table of 435 observations (of 2 variables)
+new_data_frame_3 = dplyr::mutate(new_data_frame_2, percentile = (rank/435)*100)
+percentile_top = dplyr::filter(new_data_frame_3, new_data_frame_3$percentile >= 90)
 
 #3
-top_51 = dplyr::filter(new_data_frame_3, new_data_frame_3$percentile > 51 & new_data_frame_3$percentile < 52)
+percentile_51 = dplyr::filter(new_data_frame_3, new_data_frame_3$percentile > 51 & new_data_frame_3$percentile < 52)
 
 #4
-vote_avg_top90 = mean(top_90$vote_average)
+vote_avg_top = mean(percentile_top$vote_average)
 
 #5
-vote_avg_top51 = mean(top_51$vote_average) 
-vote_avg_90_50_diff = vote_avg_top90 - vote_avg_top51
+vote_avg_51 = mean(percentile_51$vote_average) 
+vote_avg_51top_diff = vote_avg_51 - vote_avg_top
